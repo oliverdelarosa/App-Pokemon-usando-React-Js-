f7505c3f44de6da36e80100b5750c14e56b03779
oliver de la rosa <oliverdelarosavictorio>
2022-06-23 15:07:26 -0500

    estoy mandando mi archivo actualizado

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hola bienvenido al curso de react de oliver de la rosa 
+          Hola bienvenido al curso de react de oliver de la rosa 
         </p>
         {holamundo(nombre, 12)}
         <section className='componentes'>
